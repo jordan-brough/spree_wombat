51e6460a781359a9172b9b5790ee2b2836a2abf6
Andrew Thal <athal7@me.com>
2014-07-24 18:24:54 -0400

    Initial endpoint logic to create a customer return

--- Gemfile
+++ Gemfile
@@ -1,12 +1,13 @@
 source 'https://rubygems.org'
 
-gem 'spree', github: 'spree/spree', branch: "2-2-stable"
+gem 'spree', github: 'bonobos/spree', branch: "customer-returns"
 
 group :test do
   #gem 'test_after_commit'
   gem 'hub_samples', github: "spree/hub_samples", branch: "master"
   gem "pry"
   gem "pry-byebug"
+  gem "timecop"
 end
 
 gemspec
--- lib/spree/wombat/handler/add_customer_return_handler.rb
+++ lib/spree/wombat/handler/add_customer_return_handler.rb
@@ -6,12 +6,55 @@ module Spree
         def process
           return response("Please provide a customer_return payload", 400) if customer_return_params.blank?
 
-          # TODO do something real
-          response "Customer return was added", 200
+          customer_return = CustomerReturn.new(stock_location: stock_location, return_items: return_items)
+
+          if customer_return.return_items.length != intended_quantity
+            raise "Unable to create the requested amount of return items"
+          end
+
+          if customer_return.save
+            # TODO figure out how to refund
+            response "Customer return #{customer_return.id} was added", 200
+          else
+            response "Customer return could not be created, errors: #{customer_return.errors.full_messages}", 400
+          end
+        rescue => e
+          response "Customer return could not be created, errors: #{e}", 500
+        end
+
+        private
+
+        def stock_location
+          StockLocation.find_by!(name: customer_return_params[:stock_location])
+        end
+
+        def return_items
+          customer_return_params[:items].flat_map do |item|
+            order = Order.includes(inventory_units: [{ return_items: :return_authorization }, :variant]).find_by(number: item[:order_number])
+            inventory_units = order.inventory_units.select { |iu| iu.variant.sku == item[:sku] }
+            return_items = inventory_units.map(&:current_or_new_return_item)
+            return_items = prune_received_return_items(return_items)
+            return_items = sort_return_items(return_items)
+            return_items.take(item[:quantity].presence || 1)
+          end.compact
         end
 
         def customer_return_params
           @payload[:customer_return]
+        end
+
+        def intended_quantity
+          customer_return_params[:items].map { |i| i[:quantity] }.sum
+        end
+
+        def prune_received_return_items(return_items)
+          return_items.select { |ri| !ri.received? }
+        end
+
+        def sort_return_items(return_items)
+          return_items = return_items.sort { |ri| -(ri.created_at || DateTime.now).to_i }
+          return_items = return_items.sort { |ri| ri.return_authorization.try(:number) == customer_return_params[:rma] ? 0 : 1 }
+          return_items.sort { |ri| ri.persisted? ? 0 : 1 }
         end
       end
     end
