ef79cce65a5b40574befae0d44b97871f3422463
Andrew Thal <athal7@me.com>
2014-07-24 12:13:09 -0400

    Add initial customer return handler

---
+++ lib/spree/wombat/handler/add_customer_return_handler.rb
@@ -0,0 +1,19 @@
+module Spree
+  module Wombat
+    module Handler
+      class AddCustomerReturnHandler < Base
+
+        def process
+          return response("Please provide a customer_return payload", 400) if customer_return_params.blank?
+
+          # TODO do something real
+          response "Customer return was added", 201
+        end
+
+        def customer_return_params
+          @payload[:customer_return]
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/spree/wombat/handler/add_customer_return_handler_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+module Spree
+  module Wombat
+    describe Handler::AddCustomerReturnHandler do
+
+      context "#process" do
+        let(:handler) { Handler::AddCustomerReturnHandler.new(message.to_json) }
+        let(:responder) { handler.process }
+
+        context "with customer_return payload" do
+          let(:message) { { customer_return: 1 } }
+
+          it "returns a Hub::Responder" do
+            expect(responder.class.name).to eql "Spree::Wombat::Responder"
+          end
+
+          it "has the correct request_id" do
+            expect(responder.request_id).to eql message["request_id"]
+          end
+
+          it "succeeds" do
+            expect(responder.summary).to eql "Customer return was added"
+            expect(responder.code).to eql 201
+          end
+        end
+
+        context "without customer_return payload" do
+          let(:message) { { a: 1 } }
+
+          it "returns a Hub::Responder" do
+            expect(responder.class.name).to eql "Spree::Wombat::Responder"
+          end
+
+          it "has the correct request_id" do
+            expect(responder.request_id).to eql message["request_id"]
+          end
+
+          it "fails" do
+            expect(responder.summary).to eql "Please provide a customer_return payload"
+            expect(responder.code).to eql 400
+          end
+        end
+      end
+
+    end
+  end
+end
